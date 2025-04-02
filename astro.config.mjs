0636baf5939233aa33df0d66ceeade7f223bd7cb
Thunderent <robert.nechitelea@protonmail.com>
2025-04-03 00:24:08 +0300

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://your-blog-domain.com",
-  integrations: [mdx()]
+  site: "https://nrobert-dev.github.io/syntax-and-stories",
+  
+  integrations: [mdx(),  sitemap({
+      changefreq: 'monthly', // Blog posts are written monthly
+      priority: 0.7,        // Blog posts are important content
+    }),]
 });
